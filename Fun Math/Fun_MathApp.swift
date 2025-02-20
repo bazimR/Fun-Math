188fdaf9b1ab33b3da8752ba19e03ca915f23a7d
bazimR <rishaalmuhammed@gmail.com>
2025-02-20 09:23:05 +0530

    Initial Commit

---
+++ Fun Math/Fun_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_MathApp.swift
+//  Fun Math
+//
+//  Created by Rishal Bazim on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Fun_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fun MathTests/Fun_MathTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_MathTests.swift
+//  Fun MathTests
+//
+//  Created by Rishal Bazim on 20/02/25.
+//
+
+import Testing
+@testable import Fun_Math
+
+struct Fun_MathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
